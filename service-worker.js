a3b0d1473c038bbdb281fc510c0d916552682714
PSVArcoNorte <aaron_nerib@hotmail.com>
2025-06-16 16:29:33 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open('mapa-colima').then(function(cache) {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/styles.css',
+                '/manifest.json'
+            ]);
+        })
+    );
+});
+self.addEventListener('fetch', function(e) {
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
+        })
+    );
+});
